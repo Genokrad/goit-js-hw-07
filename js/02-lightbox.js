8a84614c2470b7f09698e111b38ac9599a907b76
Genokrad <zeeper90@gmail.com>
2022-10-15 00:04:50 +0300

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,21 +1,26 @@
 import { galleryItems } from "./gallery-items.js";
+
 // Change code below this line
 
 const gallerySelector = document.querySelector(".gallery");
 
-const galleryItemsArr = [];
-
 console.log(galleryItems);
-galleryItems.map((item) => {
-  const galleryItem = `<a class="gallery__item" href="${item.original}">
-  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
-</a>`;
-  galleryItemsArr.push(galleryItem);
-});
 
-gallerySelector.insertAdjacentHTML("beforeend", galleryItemsArr.join(""));
+const galleryItemsArr = galleryItems
+  .map(
+    (item) => `<a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>`
+    // return galleryItem;
+    // galleryItemsArr.push(galleryItem);
+  )
+  .join("");
+
+gallerySelector.insertAdjacentHTML("beforeend", galleryItemsArr);
 
 const lightbox = new SimpleLightbox(".gallery a", {
   captionsData: "alt",
   captionDelay: 250,
 });
+
+console.log(galleryItemsArr);
